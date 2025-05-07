9675c7f000c6b0310729f2a9c74448b720041e5a
nehad-elsayed <nehad.boody74@gmail.com>
2025-05-07 15:16:29 +0300

    Design Progress

--- js/index.js
+++ js/index.js
@@ -2,12 +2,9 @@
 
 const formElement = document.querySelector("form");
 const formInput = document.querySelector(".customInput");
-const Loading = document.querySelector(".loading")
+const Loading = document.querySelector(".loading");
 let myTodoList = [];
 let apiKey = "6819330260a208ee1fdf5db0";
-
-
-
 
 // to get and display data when the user open the app  we call getAllTodos here in global
 getAllTodos();
@@ -16,11 +13,10 @@ getAllTodos();
 formElement.addEventListener("submit", (e) => {
   e.preventDefault();
 
-if(formInput.value.trim().length > 0 ){
-  addTodo();
-}
-//don't add the task if the title is empty 
-
+  if (formInput.value.trim().length > 0) {
+    addTodo();
+  }
+  //don't add the task if the title is empty
 });
 
 //functionnnnnssssssssss =====>>>>>
@@ -39,8 +35,8 @@ async function addTodo() {
       "content-type": "application/json",
     },
   };
-  
-  showLoading()
+
+  showLoading();
   let response = await fetch(
     "https://todos.routemisr.com/api/v1/todos",
     objConfig
@@ -57,11 +53,12 @@ async function addTodo() {
     //   toastr.error("you can't add an empty task ");
     // } // لغيت الالس هنا عشان مبعتش ريكويست زياده للباك اند يفضل ان انا اعمل تشيك علي التايتل ف الاول ف الايفنت سابميت
   }
-  hideLoading() // ف الحالتين سواء اتنفذ او لا اخفي اللودنج
+  hideLoading(); // ف الحالتين سواء اتنفذ او لا اخفي اللودنج
 }
 
 // 2) functions getAllTodos and display them ====>>>>>>
 async function getAllTodos() {
+  showLoading()
   const response = await fetch(
     `https://todos.routemisr.com/api/v1/todos/${apiKey}`
   );
@@ -73,7 +70,9 @@ async function getAllTodos() {
     console.log(myTodoList.reverse());
 
     displayTodos();
+   
   }
+   hideLoading()
 }
 
 function displayTodos() {
@@ -85,7 +84,6 @@ function displayTodos() {
                 ? ` <span onclick="todoCompleted('${myTodoList[i]._id}')" style="text-decoration: line-through;"  class="taskName"> ${myTodoList[i].title} </span>`
                 : ` <span  onclick="todoCompleted('${myTodoList[i]._id}')" class="taskName"> ${myTodoList[i].title}  </span>`
             }
-              <p class="date">${myTodoList[i].createdAt.split("T", 1)}</p>
               <div class="d-flex align-items-center justify-content-center gap-3">
                 <span class="trashicon" onclick="deleteTodo('${
                   myTodoList[i]._id
@@ -104,7 +102,7 @@ function displayTodos() {
 }
 
 // 3) function to delete Todo   ====>>>
- function deleteTodo(Id) {
+function deleteTodo(Id) {
   Swal.fire({
     title: "Are you sure?",
     text: "You won't be able to revert this!",
@@ -112,15 +110,14 @@ function displayTodos() {
     showCancelButton: true,
     confirmButtonColor: "#3085d6",
     cancelButtonColor: "#d33",
-    confirmButtonText: "Yes, delete it!"
-  }).then( async (result) => {
+    confirmButtonText: "Yes, delete it!",
+  }).then(async (result) => {
     if (result.isConfirmed) {
-
+      showLoading();
       const todoData = {
         todoId: Id,
       };
-    
-    
+
       const obj = {
         method: "DELETE",
         body: JSON.stringify(todoData),
@@ -128,29 +125,31 @@ function displayTodos() {
           "content-type": "application/json",
         },
       };
-    
-      const response = await fetch(`https://todos.routemisr.com/api/v1/todos`, obj);
-    
+
+      const response = await fetch(
+        `https://todos.routemisr.com/api/v1/todos`,
+        obj
+      );
+
       if (response.ok) {
         const data = await response.json();
         if (data.message == "success") {
           Swal.fire({
             title: "Deleted!",
             text: "Your file has been deleted.",
-            icon: "success"
+            icon: "success",
           });
           getAllTodos(); // to display the new list after deleting
         }
       }
 
-    
+      hideLoading();
     }
   });
-
 }
 
 // 4) function if  task completed ===>>>>
- function todoCompleted(ID) {
+function todoCompleted(ID) {
   Swal.fire({
     title: " mark as complete?",
     text: "You won't be able to change this!",
@@ -158,13 +157,14 @@ function displayTodos() {
     showCancelButton: true,
     confirmButtonColor: "#3085d6",
     cancelButtonColor: "#d33",
-    confirmButtonText: "Yes, complete it!"
-  }).then(async(result) => {
+    confirmButtonText: "Yes, complete it!",
+  }).then(async (result) => {
     if (result.isConfirmed) {
+      showLoading()
       todoData = {
         todoId: ID,
       };
-    
+
       const obj = {
         method: "PUT",
         body: JSON.stringify(todoData),
@@ -172,35 +172,32 @@ function displayTodos() {
           "content-type": " application/json",
         },
       };
-    
-      const response = await fetch("https://todos.routemisr.com/api/v1/todos", obj);
-    
+
+      const response = await fetch(
+        "https://todos.routemisr.com/api/v1/todos",
+        obj
+      );
+
       if (response.ok) {
         const data = await response.json();
         if (data.message == "success") {
           Swal.fire({
             title: "Completed!",
-            icon: "success"
+            icon: "success",
           });
           await getAllTodos();
           console.log("completed");
         }
       }
-
-   
+      hideLoading()
     }
   });
-
 }
 
 //====>>>> functions To conrtoool loooooading screeeen ===>>>
-function showLoading(){
-Loading.classList.remove("d-none")
-
-
+function showLoading() {
+  Loading.classList.remove("d-none");
 }
-function hideLoading(){
-  Loading.classList.add("d-none")
+function hideLoading() {
+  Loading.classList.add("d-none");
 }
-
-
